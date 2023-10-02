3cb85eed3f855f0e98f56e0763ebfa037f7a4a45
Transwarpcom <141041258+Transwarpcom@users.noreply.github.com>
2023-10-02 12:38:56 +0800

    Update download_latest_ota_build.sh to a14 (Not stable)

--- download_latest_ota_build.sh
+++ download_latest_ota_build.sh
@@ -14,7 +14,8 @@ print_message "Downloading OTA builds for the following devices: $(IFS=, ; echo 
 for device_name in "$@"; do # Allow multiple arguments
   last_build_url=$(curl -Ls 'https://developers.google.cn/android/ota?partial=1' | grep -Eo "\"(\S+${device_name}\S{33})?zip" | tail -1 | tr -d \")
   print_message "Downloading OTA build for ${device_name^} (\"$last_build_url\")..." debug
-  wget --tries=inf --show-progress -q "$last_build_url"
+#  wget --tries=inf --show-progress -q "$last_build_url"
+  wget --tries=inf --show-progress -q "https://dl.google.com/developers/android/udc/images/factory/cheetah_beta-upb2.230407.014-factory-d9234046.zip?hl=zh-cn"
 done
 
 print_message "Download complete" info
